7bf966f65d8fc7c8b4af1ec8304d8e987e966996
regstrtn <regstrtn@gmail.com>
2016-08-02 19:53:45 +0530

    update myps.c

--- myps.c
+++ myps.c
@@ -32,6 +32,7 @@ int main(int argc, char* argv[]) {
 	int numuid;
 	char cmdfile[300];
 	char *cmdbuffer;
+	printf("PID TTY TIME CMD\n");
 	while((de = readdir(dr))!=NULL) {
 		strcpy(filetoread, "/proc/");
 		strcat(filetoread, de->d_name);
@@ -43,16 +44,20 @@ int main(int argc, char* argv[]) {
 			uidfound = 0;
 			if((fp = fopen(filetoread, "r")) == NULL) printf("File not found");
 			if((fp2 = fopen(cmdfile, "r"))==NULL) printf("File not found");
-			else {getline(&cmdbuffer, &bufsize, fp2);
-			printf("%s %s\n", filetoread, cmdbuffer);}
+			else {
+				getline(&cmdbuffer, &bufsize, fp2);
+			}
 			//printf("Is digit: %d %d\n", atoi(de->d_name), isnum(de->d_name));
 			stat(de->d_name, &st);
 			//printf("%c\n", fgetc(fp));
 			while(getline(&buffer, &bufsize, fp)>0) {
 				if(strncmp(buffer, "Uid", 3)==0) { 
-					printf("%s\n", buffer);
+					//printf("%s\n", buffer);
 					cuid = strtok(buffer, "\t"); cuid = strtok(NULL, "\t");
-					printf("userid: %d\n", atoi(cuid));
+					if(atoi(cuid)==501) {
+					//printf("userid: %d", atoi(cuid));
+					printf("%s %s\n", de->d_name, cmdbuffer);
+					}
 				} 
 			}
 		}
